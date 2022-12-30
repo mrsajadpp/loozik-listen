546bb49629bf6deacf0573f206bfbe88fdbdf10e
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-12-30 10:22:35 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1-min",
+    src: "music-1"
+  },
+  {
+    name: "Mehbooba - Kgf",
+    artist: "MRT Music",
+    img: "music-11-min",
+    src: "music-11"
+  }
+  ];
